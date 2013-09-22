10ae6ca9e3b730a71bb80c3dd3773a836c161a6a
startcode <lidong.main@gmail.com>
2013-09-22 13:57:15 -0400

    Create config-bash.sh

---
+++ config-bash.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "alias update='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y && sudo apt-get clean'" >> ~/.bashrc
+echo "alias pull='git pull origin master'" >>~/.bashrc
+echo "alias push='git push origin master'" >>~/.bashrc
+echo "alias die='sudo shutdown -P 0' >> ~/.bashrc
+echo "set -o vi" >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get update
+sudo apt-get install git vim
