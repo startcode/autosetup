30d7acf5d7533bd7d1e563ae0d871c845f39f1b1
Dong Li <lidong.main@gmail.com>
2018-10-20 17:59:45 -0700

    move conf files to dot files

--- gs.py
+++ gs.py
@@ -13,19 +13,19 @@ def is_copy_type(v_type):
 # gen_getter
 def gen_getter(v_type, v_name):
     if not is_copy_type(v_type):
-        const_getter = "const %s& %s() const { return _%s; }\n"% (v_type, v_name, v_name)
-        mutable_getter = "%s& %s() { return _%s; }" % (v_type, v_name, v_name)
+        const_getter = "const %s& %s() const { return %s_; }\n"% (v_type, v_name, v_name)
+        mutable_getter = "%s& %s() { return %s_; }" % (v_type, v_name, v_name)
         return const_getter + mutable_getter;
     else:
         return \
-"""%s %s() const { return _%s; }""" % (v_type, v_name, v_name)
+"""%s %s() const { return %s_; }""" % (v_type, v_name, v_name)
 
 # function to generate setter
 def gen_setter(v_type, v_name):
     if not is_copy_type(v_type):
         v_type = "const %s&" % v_type;
     return \
-"""void set_%s(%s %s) { _%s = %s; }""" % (v_name, v_type, v_name, v_name, v_name)
+"""void set_%s(%s %s) { %s_ = %s; }""" % (v_name, v_type, v_name, v_name, v_name)
 
 def read_input(f):
     for line in f.read().split("\n"):
@@ -37,14 +37,10 @@ def read_input(f):
             continue
         v_type = ' '.join(line[0:-1])
         v_name = line[-1].rstrip(";")
-        if v_name[0] != "_":
-            print "name[%s] must start with underscore" % (v_name)
-            return None
-        v_name = v_name[1:]
         getter = gen_getter(v_type, v_name)
         setter = gen_setter(v_type, v_name)
-        print getter
-        print setter
+        print(getter)
+        print(setter)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="get c++ getter and setter", prog="gs.py")
